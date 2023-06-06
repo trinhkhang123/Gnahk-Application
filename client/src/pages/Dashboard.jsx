50fd52dc3ccf914cbb71c1a1c7d1f8d900ec4354
trinhkhang123 <85794124+trinhkhang123@users.noreply.github.com>
2023-06-06 12:50:44 +0700

    update

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -18,6 +18,7 @@ export default function Dashboard() {
             key1: token
           }
         })
+        
         .then(response => {
           setUser(response.data);
           // Tiếp tục xử lý dữ liệu người dùng
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const cors = require('cors');
-const {test, registerUser,loginUser,getProfile} = require('../controllers/authController')
+const {test, registerUser,loginUser,getProfile, accountUser} = require('../controllers/authController')
 
 router.use(
     cors({
@@ -17,5 +17,7 @@ router.post('/register',registerUser);
 router.post('/login',loginUser);
 
 router.get('/profile',getProfile);
+
+router.get('/accountUser',accountUser);
 
 module.exports = router
